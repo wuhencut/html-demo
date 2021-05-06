6fecd6e7ac7686fbdd6d3c75ea0298ceb44f70bc
stone <stone_rivers@163.com>
2021-05-06 15:07:24 +0800

    测试TS基本语法

---
+++ test.js
@@ -0,0 +1,6 @@
+var a = "aaa";
+var b = function (a, b) {
+    console.log(a);
+    console.log(b + "");
+};
+b('12', 23);
---
+++ test.ts
@@ -0,0 +1,6 @@
+let a:String = "aaa"
+let b: Function = function(a: String, b: Number) {
+  console.log(a);
+  console.log(b + "")
+}
+b('12', 23)
